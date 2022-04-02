8f2950e4ccce07ede48372ac8d661bce018ba245
michel-mata <sm75@princeton.edu>
2022-04-02 13:25:01 -0400

    check reputations equilibrate

--- equilibrium-reputations.jl
+++ equilibrium-reputations.jl
@@ -1,16 +1,15 @@
 begin
-    num_workers = 8
-    include("./setup.jl")
-end
-
-@everywhere  begin
+    using Pkg
+    Pkg.activate(".")
+    using Revise
+    using GroupReputations
     using DelimitedFiles, JLD, SharedArrays
-    # using Plots
+    using Plots
     using Statistics
 end
 
 begin
-    simulation = "PIP"
+    simulation = "equilibrium-reputations"
     # Population size
     N    = 50
     # Payoff parameters
@@ -29,7 +28,6 @@ begin
     # Population parameters
     strats_weights = 1.0
     prob_weights = 1.0
-    burn_in = true
     group_sizes = [0.5,0.5]
     initial_strategies = [0]
     evolving_strategies = [0]
@@ -44,27 +42,16 @@ begin
     generations = 100N
     repetitions = 2num_workers
     norm = social_norms[1]
-    # c1,c2,c3,c4,r1,r2 = palette(:tab10)[[1,end,3,end-1,2,5]]
+    c1,c2,c3,c4,r1,r2 = palette(:tab10)[[1,end,3,end-1,2,5]]
 end
 
-
-index = [ (norm,ir,gr,p0,p1) for norm in [norm...],
-                          ir in [ind_reps_scale...],
-                          gr in [grp_reps_scale...],
-                          p1 in [invaders...],
-                          p0 in [residents...]][:]
 
 for ir in [ind_reps_scale...], sr in [grp_reps_scale...]
     data = []
     for p1 in [invaders...], p0 in [residents...]
         "running -- reps$ir stpyes$sr -- res $p0 inv $p1"|>println
         all_probs = [p0,p1]
-        # Array for trajectories
-        # reps = SharedArray{Float64,2}(repetitions,8)
-        # fitn = SharedArray{Float64,2}(repetitions,2)
 
-        Reps = SharedArray{Float64,2}(repetitions,8)
-        Fitn = SharedArray{Float64,2}(repetitions,2)
         # Path of results
         path  = "$simulation/"*
                 "$norm-$ir$sr/"*
@@ -72,80 +59,57 @@ for ir in [ind_reps_scale...], sr in [grp_reps_scale...]
                 "a$(game_pars[end])-bc$(game_pars[1]/game_pars[2])/"
         !ispath("results/"*path) && mkpath("results/"*path)
 
-        @sync @distributed for r in 1:repetitions
-            pop_file = "results/" * path * "pop_$r.jld"
-            # If population doesn't exist, create it
-            if !isfile(pop_file)
-                pop  = random_population_invasion( N, game_pars, norm,
-                                        [initial_strategies...],
-                                        [evolving_strategies...],
-                                        [all_probs...],
-                                        mutation,
-                                        ir, sr,
-                                        [strats_weights...],
-                                        [prob_weights...],
-                                        burn_in,
-                                        group_sizes)
-                g0 = 0
-                burn_in && [ play!(pop) for _ in 1:100N]
-            else
-                # If it exists, load, check gens, load prev states
-                pop = load(pop_file,"pop")
-                (pop.generation >= generations) && continue
-                g0 = pop.generation
-                # Load previous states if exist
-                # states = readdlm("data/"*path*"states.csv",',')
-                # ps[r,1:g0] = states[r,:]
-            end
+        for r in 1:repetitions
 
-            gens = generations-g0
+            pop  = random_population_invasion( N, game_pars, norm,
+                                    [initial_strategies...],
+                                    [evolving_strategies...],
+                                    [all_probs...],
+                                    mutation,
+                                    ir, sr,
+                                    [strats_weights...],
+                                    [prob_weights...],
+                                    burn_in,
+                                    group_sizes)
 
-            # reputations = SharedArray{Float64,2}(gens,4pop.num_groups)
-            # fitness = SharedArray{Float64,2}(gens,pop.num_probabilities)
+            reputations = SharedArray{Float64,2}(gens,pop.num_groups)
+            stereotypes = SharedArray{Float64,2}(gens,pop.num_groups)
+            fitness = SharedArray{Float64,2}(gens,pop.num_probabilities)
 
             # Dynamics
-            for gen in 1:gens
+            for gen in 1:generations
                 play!(pop)
                 track!(pop)
                 # Save states
-                # reputations[gen,:] = [pop.tracker.reps_ind...,pop.tracker.reps_grp...]
-                # reputations[gen,:] = reputations[gen,[1,4,2,3,5,8,6,7]]
-                # fitness[gen,:] = pop.tracker.fitn_probabilities
+                reputations[gen,:] = pop.tracker.reps_ind[:][[1,4,2,3]]
+                stereotypes[gen,:] pop.tracker.reps_grp[:][[5,8,6,7]]
+                fitness[gen,:] = pop.tracker.fitn_probabilities
             end
-            # Save Population
-            save(pop_file, "pop", pop)
-            # reps[r,:] = mean(reputations[end-burn:end,:],dims=1)
-            # fitn[r,:] = mean(fitness[end-burn:end,:],dims=1)
+            # Save trajectories
+            writedlm("results/"*path*"reputations_$r.csv",reputations,',')
+            writedlm("results/"*path*"stereotypes_$r.csv",stereotypes,',')
+            writedlm("results/"*path*"fitness_$r.csv",fitness,',')
 
-            Reps[r,:] = [pop.tracker.reps_ind...,pop.tracker.reps_grp...][[1,4,2,3,5,8,6,7]]
-            Fitn[r,:] = pop.tracker.fitn_probabilities
+            # Save Plots
+            p_r = plot(reputations[:,1:4],label=["r11" "r22" "r12" "r21"],yrange=(0,1),
+                    title="reputations",frame=:box,w=2,
+                    line=[:solid :dash :solid :dash],
+                    color=[c1 c2 c3 c4],legend=:outerright)
+            savefig(p_r,"data/"*path*"reputations_$r.pdf")
 
-            # p_r = plot(reputations[:,1:4],label=["r11" "r22" "r12" "r21"],yrange=(0,1),
-            #         title="reputations",frame=:box,w=2,
-            #         line=[:solid :dash :solid :dash],
-            #         color=[c1 c2 c3 c4],legend=:outerright)
-            #
-            #
-            # p_s = plot(reputations[:,5:end], label=["s11" "s22" "s12" "s21"],yrange=(0,1),
-            #         title="stereotypes",frame=:box,w=2,
-            #         line=[:solid :dash :solid :dash],
-            #         color=[c1 c2 c3 c4],legend=:outerright)
-            #
-            # p_f = plot(fitness, label=["res" "inv"],
-            #         title="fitness",frame=:box,w=2,
-            #         color=[r1 r2],legend=:outerright)
-            #
-            # p_f|>display
+            p_s = plot(reputations[:,5:end], label=["s11" "s22" "s12" "s21"],yrange=(0,1),
+                    title="stereotypes",frame=:box,w=2,
+                    line=[:solid :dash :solid :dash],
+                    color=[c1 c2 c3 c4],legend=:outerright)
+            savefig(p_r,"data/"*path*"stereotypes_$r.pdf")
+
+            p_f = plot(fitness, label=["res" "inv"],
+                    title="fitness",frame=:box,w=2,
+                    color=[r1 r2],legend=:outerright)
+            savefig(p_r,"data/"*path*"fitness_$r.pdf")
+
+
             "$r done!"|>println
         end
-        # Save trajectories
-        res, inv = mean(Fitn,dims=1)
-        push!(data,[p0,p1,inv-res,mean(Reps,dims=1)...])
-        # !ispath("data/"*path) && mkpath("data/"*path)
-        # writedlm("data/"*path*"reps.csv",reputations,',')
     end
-    path  = "$simulation/"*
-            "$norm-$ir$sr/"
-    !ispath("data/"*path) && mkpath("data/"*path)
-    writedlm("data/"*path*"data.csv",hcat(data...)',',')
 end
