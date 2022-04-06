c40ec6031faaefec1b9b36fefd68c60de0a22c05
michel-mata <sm75@princeton.edu>
2022-04-05 20:47:57 -0400

    exit instead of continue

--- adaptive-dynamics.jl
+++ adaptive-dynamics.jl
@@ -94,7 +94,7 @@
         # More gens than requested break
         if pop.generation > generations
             "$ir-$sr-$p0-$r done! -- "|>println
-            @goto escape_label
+            exit()
             "error"|>println
         end
         # Less gens, load old probabilities
@@ -115,5 +115,4 @@
     save("results/"*path*"pop_$r.jld", "pop", pop)
     # Save trajectories
     writedlm("results/"*path*"probabilities_$r.csv",probabilities,',')
-    @label escape_label
 "done!" |> println
