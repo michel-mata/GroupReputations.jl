1d8c3cdbb46064c6acf53c409251a412926ddd57
michel-mata <sm75@princeton.edu>
2022-04-05 20:51:22 -0400

    var dims

--- adaptive-dynamics.jl
+++ adaptive-dynamics.jl
@@ -71,7 +71,7 @@
     !ispath("results/"*path) && mkpath("results/"*path)
     !ispath("data/"*path) && mkpath("data/"*path)
 
-    probabilities = SharedArray{Float64,2}(generations)
+    probabilities = SharedArray{Float64,1}(generations)
 
     pop_file = "results/" * path * "pop_$r.jld"
     # If population doesn't exist, create it
